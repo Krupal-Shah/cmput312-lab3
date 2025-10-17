c0b6eb3ff7d93b7dbbc25f6491998c5b390d30a1
Krupal Shah <krupalhi@ualberta.ca>
2025-10-17 17:39:45 -0600

    Added client

--- lab3_problem2-client.py
+++ lab3_problem2-client.py
@@ -11,16 +11,30 @@ port = 9999
 client = client.Client(host, port)
 SPEED = 50
 
+
 def main():
     while True:
         data = client.pollData()
-        if len(data) > 0 and data.startswith("Angle"):
-            data = data.split(" ")
-            angle1, angle2 = float(data[1]), float(data[2])
-            ut.move_to_angles(SPEED, [angle1, angle2])
-            client.sendDone()
-        # Process the received data and perform necessary actions
+        if len(data) > 0:
+            if data == "EXIT":
+                print("Termination signal received. Exiting...")
+                break
+            if data == "SAFETY_ON":
+                v.SAFETY_MODE = True
+                print("Safety mode enabled.")
+                continue
+            if data == "SAFETY_OFF":
+                v.SAFETY_MODE = False
+                print("Safety mode disabled.")
+                continue
+            if data.startswith("MOVE"):
+                data = data.split(" ")
+                angle1, angle2 = float(data[1]), float(data[2])
+                ut.move_to_angles(SPEED, [angle1, angle2])
+                client.sendDone()
+
         sleep(0.2)
+
 
 if __name__ == "__main__":
     main()
